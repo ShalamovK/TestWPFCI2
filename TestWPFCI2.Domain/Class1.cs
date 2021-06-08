e3b40cf8cd3160524b3317be2359cd1123ec0b13
Kirill <shalam.kirill@gmail.com>
2021-06-09 00:58:47 +0700

    test41

--- TestWPFCI2.Domain/Class1.cs
+++ TestWPFCI2.Domain/Class1.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TestWPFCI2.Domain
 {
     public class Class1
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(Class1));
+        public string json = JsonConvert.SerializeObject(new Foo {
+            A = "10"
+        });
+    }
 
+    public class Foo {
+        public string A { get; set; }
     }
 }
